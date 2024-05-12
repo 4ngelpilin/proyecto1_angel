29ee9e90933eb516f6df6e8d72274236a3895d47
4ngelpilin <angelhernandez13985@gmail.com>
2024-05-11 18:47:04 -0600

    iniciar proyecto

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <?php
+        echo "<h1> Titulo del blok </h1>";
+        echo "<p> En un lugar de tejalpa cuyo nombre no quiero recordar  </p>";
+        $i = 0;
+        for ($i=0; $i<10; $i++){
+            echo $i." <br>";
+        }
+
+        $i = 2;
+        for ($i=2; $i<11; $i++){
+            echo $i++." ";
+        }
+    ?>
+</html>    
